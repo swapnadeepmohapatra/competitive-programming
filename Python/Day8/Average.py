525fc25e23471bb0cc89ac01c97d67090c47df81
SwapnadeepMohapatra <swapnadeep456@gmail.com>
2019-07-29 13:17:26 +0530

    Day8

---
+++ Python/Day8/Average.py
@@ -0,0 +1,12 @@
+howard = 8
+pease = 10
+campbell = 9
+grace = 8
+mcCarthy = 7
+murphy = 12
+
+totalNutse = 6
+
+average = float((howard + pease + campbell + grace + mcCarthy + murphy)/totalNutse)
+
+print(average)
